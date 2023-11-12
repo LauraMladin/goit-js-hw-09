78ad71e47c89ca7e9da941a818b0ee099eec666f
LauraMladin <laura.mladin95@gmail.com>
2023-11-12 13:18:16 +0200

    Update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyColor = document.querySelector('body');
 
+let intervalId = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+
+  stopBtn.setAttribute('disabled', '');
+
+  startBtn.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+
+    intervalId = setInterval(() => {
+        bodyColor.style.background = getRandomHexColor();
+    }, 1000);
+  })
+
+  stopBtn.addEventListener('click', element => {
+    element.target.setAttribute('disabled', true);
+    startBtn.removeAttribute('disabled');
+
+    clearInterval(intervalId);
+  });
